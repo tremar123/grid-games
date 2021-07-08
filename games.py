bc7d810439cc95e1132333bc1d9bf866abff1d93
tremar123 <marcelmalych5@gmail.com>
2021-07-08 13:53:51 +0200

    first game in progress

--- games.py
+++ games.py
@@ -3,6 +3,6 @@ from flask import render_template
 from helpers import login_required
 
 @app.route("/memory-game")
-@login_required
+# @login_required
 def memory():
     return render_template("memory-game.html")
