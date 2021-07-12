001b54abb16899d9cc3fa222dbaf2be0beae7180
tremar123 <marcelmalych5@gmail.com>
2021-07-12 09:03:08 +0200

    whac-a-mole init

--- games.py
+++ games.py
@@ -4,5 +4,10 @@ from helpers import login_required
 
 @app.route("/memory-game")
 # @login_required
-def memory():
+def memory_game():
     return render_template("memory-game.html")
+
+@app.route("/whac-a-mole")
+# @login_required
+def whac_a_mole():
+    return render_template("whac-a-mole.html")
